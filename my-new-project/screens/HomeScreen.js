507d03c94c9aeb88545d0fb199507b9c8b7afd00
Krimmekus <krimmekus@crimson.ua.edu>
2019-03-23 17:55:08 -0500

    Update HomeScreen.js

--- my-new-project/screens/HomeScreen.js
+++ my-new-project/screens/HomeScreen.js
@@ -42,7 +42,7 @@ export default class HomeScreen extends React.Component {
             </View>
 
             <Text style={styles.getStartedText}>
-              Hello World. I am the captain now.
+              Hello World.
             </Text>
           </View>
 
