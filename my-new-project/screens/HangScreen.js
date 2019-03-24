da7ac4fa5248d3beefbde2eb722530dddc28cb72
Krimmekus <krimmekus@crimson.ua.edu>
2019-03-24 02:48:02 -0500

    stuff

--- my-new-project/screens/HangScreen.js
+++ my-new-project/screens/HangScreen.js
@@ -16,6 +16,8 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
+let checkedIn=false;
+
 export default class HangScreen extends React.Component {
   static navigationOptions = {
     header: null,
@@ -33,65 +35,64 @@ export default class HangScreen extends React.Component {
   };
 
   render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-          </View>
+    if (checkedIn == false) {
+      return (
+        <View style={styles.container}>
+          <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+            <View style={styles.welcomeContainer}>
+              <Image
+                source={
+                  __DEV__
+                    ? require('../assets/images/robot-dev.png')
+                    : require('../assets/images/robot-prod.png')
+                }
+                style={styles.welcomeImage}
+              />
+            </View>
 
-          <View style={styles.getStartedContainer}>
-            {/*}{this._maybeRenderDevelopmentModeWarning()}*/}
-            {/*}<View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+            <View style={styles.getStartedContainer}>
+              {/*}{this._maybeRenderDevelopmentModeWarning()}*/}
+              {/*}<View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+                <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
+              </View>*/}
+
+              <Text
+                style={{ fontSize: 24, textAlign: 'center' }}
+              >{"\n\n"}Show your friends where you are!{"\n"}</Text>
+
+              <TextInput
+                style={{ fontSize: 24, borderColor: 'lightgray', borderWidth: 2 }}
+                textAlign={'center'}
+                multiline={true}
+                placeholder="What's your location?"
+                onChangeText={(text) => this.setState({text})}
+              />
+
+              <Text style={{ fontSize: 24 }}>{"\n\n"}</Text>
+
+              <Button
+                title="Check In"
+                onPress={ checkedIn=true }
+              />
+            </View>
+
+            {/*}<View style={styles.helpContainer}>
+              <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
+                <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
+              </TouchableOpacity>
             </View>*/}
+          </ScrollView>
 
-            <Text
-              style={{ fontSize: 24, textAlign: 'center' }}
-            >{"\n\n"}Show your friends where you are!{"\n"}</Text>
+          {/*}<View style={styles.tabBarInfoContainer}>
+            <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
 
-            <TextInput
-              style={{ fontSize: 24, borderColor: 'lightgray', borderWidth: 2 }}
-              textAlign={'center'}
-              multiline={true}
-              placeholder="What's your location?"
-              onChangeText={(text) => this.setState({text})}
-            />
-
-            <Text style={{ fontSize: 24 }}>{"\n\n"}</Text>
-
-            <Button
-              title="Share Location"
-
-              onPress={() => {
-                
-              }}
-            />
-          </View>
-
-          {/*}<View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
+            <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+              <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
+            </View>
           </View>*/}
-        </ScrollView>
-
-        {/*}<View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>*/}
-      </View>
-    );
+        </View>
+      );
+    }
   }
 
   _maybeRenderDevelopmentModeWarning() {
@@ -113,6 +114,12 @@ export default class HangScreen extends React.Component {
         <Text style={styles.developmentModeText}>
           You are not in development mode, your app will run at full speed.
         </Text>
+      );
+    }
+
+    if (checkedIn == true) {
+      return(
+        <View><Text>Please Work!</Text></View>
       );
     }
   }
