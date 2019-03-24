892cf42013dd9c4d5652ee6e82872a2fd33f85b3
micahwhite17 <micah.white@live.com>
2019-03-23 22:31:15 -0500

    Very beginning of Friends List

--- my-new-project/screens/FriendsScreen.js
+++ my-new-project/screens/FriendsScreen.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, View, Text, Image } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
+import Touchable from 'react-native-platform-touchable';
+
+
+var database = {
+  'numFriends': 3,
+  'numLocations': 1,
+  'location': 'Ferg',
+  'friends': ['Micah', 'Keaton', 'Josh'],
+};
 
 export default class FriendsScreen extends React.Component {
   static navigationOptions = {
@@ -12,7 +21,21 @@ export default class FriendsScreen extends React.Component {
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */
-        //<ExpoLinksView />
+        // <ExpoLinksView />
+        }
+
+        
+          <Touchable
+            style={styles.location}
+            background={Touchable.Ripple('#ccc', false)}>
+            <View style={{ flexDirection: 'row' }}>
+              <View style={styles.locationContainer}>
+                <Text style={styles.locationText}>
+                  {database.location}
+                </Text>
+              </View>
+            </View>
+          </Touchable>
         }
 
       </ScrollView>
@@ -23,7 +46,23 @@ export default class FriendsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 15,
+    paddingTop: 0,
     backgroundColor: '#fff',
   },
+  location: {
+    backgroundColor: '#fdfdfd',
+    paddingHorizontal: 5,
+    paddingVertical: 6,
+    borderBottomWidth: 2,
+    borderBottomColor: '#EDEDED',
+  },
+  locationText: {
+    fontSize: 35,
+    marginLeft: 10,
+    marginTop: 2,
+    marginBottom: 2,
+  },
+  locationContainer: {
+    marginRight: 5
+  }
 });
